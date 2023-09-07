17167b51b9f5d96e417b54a972d7dfe3ca99c391
Andre-lucs <andreltd312@gmail.com>
2023-09-07 17:19:43 -0300

    Created FindbyId request on User

--- src/main/java/com/andrelucs/springbootmongodb/resource/UserResource.java
+++ src/main/java/com/andrelucs/springbootmongodb/resource/UserResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,6 +22,12 @@ public class UserResource {
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll(){
         var response = userService.findAll().stream().map(u -> new UserDTO(u)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping(value = "/")
+    public ResponseEntity<UserDTO> findById(@RequestParam("id") String id){
+        UserDTO response = new UserDTO(userService.findById(id));
         return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/com/andrelucs/springbootmongodb/service/UserService.java
+++ src/main/java/com/andrelucs/springbootmongodb/service/UserService.java
@@ -2,6 +2,7 @@ package com.andrelucs.springbootmongodb.service;
 
 import com.andrelucs.springbootmongodb.domain.User;
 import com.andrelucs.springbootmongodb.repository.UserRepository;
+import com.andrelucs.springbootmongodb.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +15,12 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(String id){
+        User u = userRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("User not found"));
+
+        return u;
     }
 }
